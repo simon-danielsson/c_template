cd82f4c8d14ca1305aa42b89135c67ca58109c23
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-07-25 13:42:25 +0200

    docs: license/program information code inline

--- init.py
+++ init.py
@@ -1,5 +1,40 @@
 #!/usr/bin/env python3
 
+# Copyright © 2026 Simon Danielsson
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files, to deal in the Software
+# without restriction, including without limitation the rights to use, copy,
+# modify, merge, publish, distribute, sublicense, and/or sell copies of the
+# Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""
+
+- init.py -
+
+C projects without a third-party build-system.
+
+Built by Simon Danielsson
+
+Source: https://github.com/simon-danielsson/c_template
+Author: https://www.simondanielsson.se/
+
+Requirements: Python 3.10+
+
+"""
+
 import subprocess, sys, shutil
 from dataclasses import dataclass, field
 from pathlib import Path
